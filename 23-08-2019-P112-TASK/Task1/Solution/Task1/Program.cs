008cde835c566b5e01e8a076503f10f65d01a6b9
yavarguliyev <yavarrg@code.edu.az>
2019-08-24 20:52:33 +0400

    1st task is done

---
+++ 23-08-2019-P112-TASK/Task1/Solution/Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Add the numbers: ");
+            string[] str = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            int[] numbers = new int[str.Length];
+            for (int i = 0; i < str.Length; i++)
+            {
+                numbers[i] = int.Parse(str[i]);
+            }
+
+            Console.WriteLine("The largest number is: ");
+            Console.WriteLine(numbers.Max());
+            Console.WriteLine("The smallest number is: ");
+            Console.WriteLine(numbers.Min());
+            Console.WriteLine("The sum is: ");
+            Console.WriteLine(numbers.Sum());
+            Console.WriteLine("The reverse of these numbers: ");
+            Array.Reverse(numbers);
+            foreach (int i in numbers)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
